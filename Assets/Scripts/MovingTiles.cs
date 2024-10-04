cce5ef7efd426027aeac5f92636765608cc2c907
MrUniverse369 <132138248+MrUniverse369@users.noreply.github.com>
2024-10-04 11:40:15 +0100

    MovingTiles added function to have object face the direction its moving

--- Assets/Scripts/MovingTiles.cs
+++ Assets/Scripts/MovingTiles.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingTiles : MonoBehaviour
-{   [SerializeField] private Transform pointA;
+{
+    [SerializeField] private Transform pointA;
     [SerializeField] private Transform pointB;
     [SerializeField] private GameObject objToMove;
     [SerializeField] private float speed;
     public bool enemyTurn;
     private Vector3 CurTargetPos;
-    // Start is called before the first frame update
+    private Vector3 originalScale;
+
+
     void Start()
     {
         CurTargetPos = pointB.position;
-      
-
+        originalScale = objToMove.transform.localScale;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         MoveTile();
@@ -25,18 +27,36 @@ public class MovingTiles : MonoBehaviour
 
     private void MoveTile()
     {
+
         objToMove.transform.position = Vector3.MoveTowards(objToMove.transform.position, CurTargetPos, Time.deltaTime * speed);
+
+
         if (objToMove.transform.position == pointB.position)
-        {     enemyTurn = true;
-            CurTargetPos = pointA.transform.position;
-       
+        {
+            enemyTurn = true;
+            CurTargetPos = pointA.position;
         }
 
         if (objToMove.transform.position == pointA.position)
-        {  enemyTurn = false;
-            CurTargetPos = pointB.transform.position;
-          
+        {
+            enemyTurn = false;
+            CurTargetPos = pointB.position;
         }
-        
+
+        FlipObject();
+    }
+
+    private void FlipObject()
+    {
+
+        if (CurTargetPos.x < objToMove.transform.position.x)
+        {
+            objToMove.transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+
+        else if (CurTargetPos.x > objToMove.transform.position.x)
+        {
+            objToMove.transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
     }
 }
