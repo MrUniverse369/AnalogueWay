d19978274ed7dd08c36d68f7b6df9d7135c83179
MrUniverse369 <132138248+MrUniverse369@users.noreply.github.com>
2024-10-11 19:27:55 +0100

    egypt Level Design implmentation 2nd route - incomplte updated sanyshark sprite - complete updated fallingtiles on egypt level - incomplete MovingObstacles script clean up - complete

--- Assets/Scripts/MovingObstacles.cs
+++ Assets/Scripts/MovingObstacles.cs
@@ -30,7 +30,7 @@ namespace AnalogueWay
         }
         private void FixedUpdate()
         {
-            //   IsPlayerBelowObject();
+            // IsPlayerBelowObject();
             if (pDetected)
             {
                 Drop();
@@ -43,25 +43,19 @@ namespace AnalogueWay
             rb2D.velocity = new Vector2(rb2D.velocity.x, -dropSpeed);
         }
 
-        /* public void IsPlayerBelowObject()
-         {
-             //calculate the displacment between the falling object and player in the horizontal axis two objects
-             dist = transform.position.x - pRef.transform.position.x;
-             if (pRef.transform.position.x > transform.position.x) dist = dist * -1;
-             if (dist < 1 && pRef.transform.position.y < transform.position.y) pDetected = true;
-         }*/
+        /**/
+        public void IsPlayerBelowObject()
+        {
+            //calculate the displacment between the falling object and player in the horizontal axis two objects
+            dist = transform.position.x - pRef.transform.position.x;
+            if (pRef.transform.position.x > transform.position.x) dist = dist * -1;
+            if (dist < 1 && pRef.transform.position.y < transform.position.y) pDetected = true;
+        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
 
-
-            if (other.gameObject.layer == LayerMask.NameToLayer("ICC"))
-            {
-                Debug.Log("ICC DETECTED Moving Obstacles Script");
-                return;
-            }
-
-            if (other.CompareTag("FallingObjGround"))
+            if (other.CompareTag("Player"))
             {
                 transform.gameObject.SetActive(false);
             }
