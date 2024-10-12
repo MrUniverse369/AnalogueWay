b66739b16a501ba61f6b51841bee410a71ba215f
MrUniverse369 <132138248+MrUniverse369@users.noreply.github.com>
2024-10-12 20:24:06 +0100

    MovingObstacles added falling tile end possition trigger - complete player stomp detection area updated - complete

--- Assets/Scripts/MovingObstacles.cs
+++ Assets/Scripts/MovingObstacles.cs
@@ -55,7 +55,7 @@ namespace AnalogueWay
         private void OnTriggerEnter2D(Collider2D other)
         {
 
-            if (other.CompareTag("Player"))
+            if (other.CompareTag("Player") || other.CompareTag("FallingObjGround"))
             {
                 transform.gameObject.SetActive(false);
             }
